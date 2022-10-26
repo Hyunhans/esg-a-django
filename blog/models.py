646d57cf4d45f0c04cf5c1372045307ecbaa8b98
HAan <46588850+zldzhd99@users.noreply.github.com>
2022-10-26 14:18:17 +0900

    blog.Post 모델/마이그레이션 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):  #class 의 첫글짜는 항상 대문자
+
+    title = models.CharField(max_length=30)  #CharField 는 길이 제한이 있는 메소드
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField()
